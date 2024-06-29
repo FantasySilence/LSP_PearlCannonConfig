faf1c045839dbf150e87c4ae399bb377dc8b21fe
FantasySilence <1735793009@qq.com>
2024-06-30 00:44:13 +0800

    添加了主程序入口，完成UI的前端设计

---
+++ main.py
@@ -0,0 +1,11 @@
+# =================================== #
+# @Author: Fantasy_Silence            #
+# @Time: 2024-06-29                   #
+# @IDE: Visual Studio Code & PyCharm  #
+# @Python: 3.9.7                      #
+# =================================== #
+# @Descript: 这里是主程序入口的UI版     #
+# =================================== #
+from src.ui.mainFrame import MainFrame
+
+MainFrame.show()
