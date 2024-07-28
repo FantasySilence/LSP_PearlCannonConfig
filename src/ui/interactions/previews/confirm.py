362a421b2ba5bd8be39580d18f734ac44c7b8aa9
FantasySilence <1735793009@qq.com>
2024-07-29 00:58:18 +0800

    添加了高级设置界面中预览界面的功能

---
+++ src/ui/interactions/previews/confirm.py
@@ -0,0 +1,41 @@
+# ================================================= #
+# @Author: Fantasy_Silence                          #
+# @Time: 2024-07-28                                 #
+# @IDE: Visual Studio Code & PyCharm                #
+# @Python: 3.9.7                                    #
+# ================================================= #
+# @Description: 使用预览中的设置按钮的功能             #
+# ================================================= #
+import os
+import sys
+import json
+from ttkbootstrap.constants import *
+from src.common.filesio import FilesIO
+
+
+def confirm_func(cls) -> None:
+
+    """
+    使用预览中的设置按钮的功能
+    """
+
+    # ------ 获取文本内容 ------ #
+    settings_str = cls.text_area.get(1.0, END)
+
+    # ------ 清除注释与空行 ------ #
+    settings = ""
+    for line in settings_str.split('\n'):
+        if line.strip() == "" or line.strip().startswith("#"):
+            continue
+        settings += line.strip()
+            
+    # ------ 转换为字典 ------ #
+    settings_dict = eval(settings)
+
+    # ------ 存入设置文件中 ------ #
+    with open(FilesIO.load_json("settings.json"), "w", encoding="utf-8") as f:
+        json.dump(settings_dict, f, ensure_ascii=False, indent=4)
+    
+    # ------ 重启应用，设置生效 ------ #
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
