0f9957e248df31afb588a629f08e578f5a1f48cf
FantasySilence <1735793009@qq.com>
2024-07-30 15:24:44 +0800

    更新版本至v6.3，修复了部分按钮的异常处理

--- src/ui/interactions/previews/confirm.py
+++ src/ui/interactions/previews/confirm.py
@@ -19,23 +19,26 @@ def confirm_func(cls) -> None:
     使用预览中的设置按钮的功能
     """
 
-    # ------ 获取文本内容 ------ #
-    settings_str = cls.text_area.get(1.0, END)
+    try:
+        # ------ 获取文本内容 ------ #
+        settings_str = cls.text_area.get(1.0, END)
 
-    # ------ 清除注释与空行 ------ #
-    settings = ""
-    for line in settings_str.split('\n'):
-        if line.strip() == "" or line.strip().startswith("#"):
-            continue
-        settings += line.strip()
-            
-    # ------ 转换为字典 ------ #
-    settings_dict = eval(settings)
+        # ------ 清除注释与空行 ------ #
+        settings = ""
+        for line in settings_str.split('\n'):
+            if line.strip() == "" or line.strip().startswith("#"):
+                continue
+            settings += line.strip()
+                
+        # ------ 转换为字典 ------ #
+        settings_dict = eval(settings)
 
-    # ------ 存入设置文件中 ------ #
-    with open(FilesIO.load_json("settings.json"), "w", encoding="utf-8") as f:
-        json.dump(settings_dict, f, ensure_ascii=False, indent=4)
-    
-    # ------ 重启应用，设置生效 ------ #
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
+        # ------ 存入设置文件中 ------ #
+        with open(FilesIO.load_json("settings.json"), "w", encoding="utf-8") as f:
+            json.dump(settings_dict, f, ensure_ascii=False, indent=4)
+        
+        # ------ 重启应用，设置生效 ------ #
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+    except Exception:
+        pass
--- src/ui/interactions/previews/export_settings.py
+++ src/ui/interactions/previews/export_settings.py
@@ -10,32 +10,36 @@ import json
 from tkinter.constants import *
 from tkinter.filedialog import asksaveasfilename
 
+
 def export_func(cls) -> None:
 
     """
     导出设置功能
     """
+    
+    try:
+        # ------ 获取文本内容 ------ #
+        settings_str = cls.text_area.get(1.0, END)
 
-    # ------ 获取文本内容 ------ #
-    settings_str = cls.text_area.get(1.0, END)
+        # ------ 清除注释与空行 ------ #
+        settings = ""
+        for line in settings_str.split('\n'):
+            if line.strip() == "" or line.strip().startswith("#"):
+                continue
+            settings += line.strip()
+                
+        # ------ 转换为字典 ------ #
+        settings_dict = eval(settings)
 
-    # ------ 清除注释与空行 ------ #
-    settings = ""
-    for line in settings_str.split('\n'):
-        if line.strip() == "" or line.strip().startswith("#"):
-            continue
-        settings += line.strip()
-            
-    # ------ 转换为字典 ------ #
-    settings_dict = eval(settings)
-
-    # ------ 导出文件 ------ #
-    file_path = asksaveasfilename(
-        title=" ",
-        initialfile="settings.json",
-        defaultextension=".json", 
-        filetypes=[("JSON files", "*.json")]
-    )
-    if file_path:
-        with open(file_path, "w", encoding="utf-8") as f:
-            json.dump(settings_dict, f, ensure_ascii=False, indent=4) 
+        # ------ 导出文件 ------ #
+        file_path = asksaveasfilename(
+            title=" ",
+            initialfile="settings.json",
+            defaultextension=".json", 
+            filetypes=[("JSON files", "*.json")]
+        )
+        if file_path:
+            with open(file_path, "w", encoding="utf-8") as f:
+                json.dump(settings_dict, f, ensure_ascii=False, indent=4)
+    except Exception:
+        pass
